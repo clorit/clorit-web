1f5ebdd16418d940a0541a6c868d9da699952284
blueapple99 <blueapple99@yonsei.ac.kr>
2021-11-22 01:06:55 +0900

    Main 화면 중간광고

--- src/components/MainLayout_Home.js
+++ src/components/MainLayout_Home.js
@@ -14,24 +14,81 @@ const MainLayout_Home = () => {
           <div className='Item'>
             <img src={jimin} />
             <div className='Bar'>
+              <div className="arrow left"/>
               <div className='Box'>
                 <div className='cap'>
-                  <img src={home_1}/>
+                  <img className="image" src={home_1}/>
                 </div>
                 <div className='text'>
-                  <p>191121 출국</p>
-                  <p>SAINT LAURENT</p>
-                  <p>Sequin velvet Bomber jacket</p>
-                  <p>$4,590</p>
+                  <p className='title'>191121 출국</p>
+                  <p className='subtitle'>SAINT LAURENT</p>
+                  <p className='subtitle'>Sequin velvet Bomber jacket</p>
+                  <p className='subtitle'>$4,590</p>
                 </div>
               </div>
-             
+              <div className='Box'>
+                <div className='cap'>
+                  <img className="image" src={home_2}/>
+                </div>
+                <div className='text'>
+                  <p className='title'>200614 방방콘 퇴근</p>
+                  <p className='subtitle'>[NOMAD]</p>
+                  <p className='subtitle'>Human T-Shirt</p>
+                  <p className='subtitle'>$73</p>
+                </div>
+              </div>
+              <div className='Box'>
+                <div className='cap'>
+                  <img className="image" src={home_3}/>
+                </div>
+                <div className='text'>
+                  <p className='title'>201222 달려라방탄</p>
+                  <p className='subtitle'>[Carhatt]</p>
+                  <p className='subtitle'>Bib Overall Denim -Stone ...</p>
+                  <p className='subtitle'>$218</p>
+                </div>
+              </div>
+              <div className="arrow right"/>
             </div>
           </div>
           <div className='Item'>
             <img src={iu} />
             <div className='Bar'>
-
+              <div className="arrow left"/>
+              <div className='Box'>
+                <div className='cap'>
+                  <img className="image" src={home_1}/>
+                </div>
+                <div className='text'>
+                  <p className='title'>191121 출국</p>
+                  <p className='subtitle'>SAINT LAURENT</p>
+                  <p className='subtitle'>Sequin velvet Bomber jacket</p>
+                  <p className='subtitle'>$4,590</p>
+                </div>
+              </div>
+              <div className='Box'>
+                <div className='cap'>
+                  <img className="image" src={home_2}/>
+                </div>
+                <div className='text'>
+                  <p className='title'>200614 방방콘 퇴근</p>
+                  <p className='subtitle'>[NOMAD]</p>
+                  <p className='subtitle'>Human T-Shirt</p>
+                  <p className='subtitle'>$73</p>
+                </div>
+              </div>
+              <div className='Box'>
+                <div className='cap'>
+                  <img className="image" src={home_3}/>
+                </div>
+                <div className='text'>
+                  <p className='title'>201222 달려라방탄</p>
+                  <p className='subtitle'>[Carhatt]</p>
+                  <p className='subtitle'>Bib Overall Denim -Stone ...</p>
+                  <p className='subtitle'>$218</p>
+                </div>
+              </div>
+              <div className="arrow right"/>
             </div>
           </div>
         </div>
