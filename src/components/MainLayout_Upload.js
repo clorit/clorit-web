76708fe981b73e93dd04f38e001a564e54455949
Woojin Joo <jwjin95@yonsei.ac.kr>
2021-11-28 18:49:30 +0900

    Upload 뼈대

--- src/components/MainLayout_Upload.js
+++ src/components/MainLayout_Upload.js
@@ -10,6 +10,7 @@ import Grid from '@mui/material/Grid';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 const darkTheme = createTheme({
   palette: {
@@ -137,92 +138,105 @@ const MainLayout_Upload = () => {
                   </Grid>
                 </Grid>
               </div>
-                
-                
-                
+              <div className = 'container'>
+                <Grid container spacing={1}>
+                  <Grid className ='date' item>
+                      카테고리
+                  </Grid>
+                  <Grid item>
+                      <FormControl style={{ minWidth:120 }}>
+                          <InputLabel></InputLabel>
+                          <Select
+                              label='Year'
+                          >
+                          </Select>
+                      </FormControl>
+                  </Grid>
+                  <Grid item>
+                      <FormControl style={{ minWidth:120 }}>
+                          <InputLabel></InputLabel>
+                          <Select
+                              label='Month'    
+                          >
+                          </Select>
+                      </FormControl>
+                  </Grid>
+                </Grid>
+              </div>    
+              <div className = 'container'>
+                    <Grid container spacing={6}>
+                        <Grid className ='date' item>
+                            색상
+                        </Grid>
+                        <Grid item>
+                            <FormControl style={{ minWidth:250 }}>
+                              <InputLabel>색상</InputLabel>
+                                <Input
+                                    label='Color'
+                                >
+                                </Input>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+              </div> 
+              <div className = 'container'>
+                    <Grid container spacing={1}>
+                        <Grid className ='date' item>
+                            브랜드명
+                        </Grid>
+                        <Grid item>
+                            <FormControl style={{ minWidth:250 }}>
+                              <InputLabel>브랜드명</InputLabel>
+                                <Input
+                                    label='Color'
+                                >
+                                </Input>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+              </div>
+              <div className = 'container'>
+                    <Grid container spacing={6}>
+                        <Grid className ='date' item>
+                            가격
+                        </Grid>
+                        <Grid item>
+                            <FormControl style={{ minWidth:250 }}>
+                              <InputLabel>가격</InputLabel>
+                                <Input
+                                    label='Color'
+                                >
+                                </Input>
+                                
+                            </FormControl>
+                            <Select label='kind'>
+                              {/* <InputLabel>단위</InputLabel> */}
+                              <MenuItem>$</MenuItem>
+                              <MenuItem>KRW</MenuItem>
+                            </Select>
+                        </Grid>
+                    </Grid>
+              </div>
+              <div className = 'container'>
+                    <Grid container spacing={6}>
+                        <Grid className ='date' item>
+                            태그
+                        </Grid>
+                        <Grid item>
+                            <FormControl style={{ minWidth:1000 }}>
+                              <InputLabel>태그</InputLabel>
+                                <Input
+                                    label='Color'
+                                >
+                                </Input>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+              </div>
             </div>
-            <div className='box1'>
-                <div>
-                    <p className = 't1'>최근 검색한</p>
-                </div>   
-                <div className = 'bar1'>
-                    <div className='innerbox'>
-                        <p>다현 라디오스타</p>
-                    </div>
-                    <div className='innerbox'>
-                        <p>NCT127 무대의상</p>
-                    </div>
-                </div>
-            </div>    
-            <div className='box1'>
-                <div>
-                    <p className = 't1'>요즘 인기있는</p>
-                </div>   
-                <div className = 'bar1'>
-                    <div className='innerbox'>
-                        <p>인기가요 MC</p>
-                    </div>
-                    <div className='innerbox'>
-                        <p>211002 더팩트뮤직어워즈</p>
-                    </div>
-                    <div className='innerbox'>
-                        <p>세븐틴 Rock with you</p>
-                    </div>
-                    <div className='innerbox'>
-                        <p>에스파 아는형님</p>
-                    </div>
-                </div>
-                <div className = 'bar1'>
-                    <div className='innerbox'>
-                        <p>트레저</p>
-                    </div>
-                    <div className='innerbox'>
-                        <p>TXT 안무영상</p>
-                    </div>
-                </div>
-            </div> 
-            <div className='box1'>
-                <div>
-                    <p className = 't1'>클로릿이 추천하는</p>
-                </div>   
-                <div className = 'bar1'>
-                    <div className='innerbox_2'>
-                        <p className = 't2'>#SM</p>
-                        <p className = 't2'>할로윈</p>
-                    </div>
-                    <div className='innerbox_2'>
-                        <p className = 't2'>비투비</p>
-                        <p className = 't2'>우정반지</p>
-                    </div>
-                    <div className='innerbox_2'>
-                        <p className = 't2'>#BTS</p>
-                        <p className = 't2'>달려라방탄</p>
-                    </div>
-                    <div className='innerbox_2'>
-                        <p className = 't2'>#정한</p>
-                        <p className = 't2'>바닐라코</p>
-                    </div>
-                    <div className='innerbox_2'>
-                        <p className = 't2'>#미주</p>
-                        <p className = 't2'>놀면뭐하니</p>
-                    </div>
-                </div>
-            </div>
-            <div className='box1'>
-                <div>
-                    <p className = 't1'>실시간 인기 검색어</p>
-                    <hr size='2' width='1400px' color='#626060'/>
-                    <div className='pBox'>
-                        <div className = 'pBox1'>
-                            <p className = 't3'>1</p>
-                        </div>
-                        <div className = 'pBox2'>
-                            <p className = 't4'>210428 팬미팅 지민</p>
-                        </div>
-                    </div>
-                    <hr size='2' width='1400px' color='#626060'/>
-                </div>   
-            </div>       
+                  
+
+
         </div>
     </ThemeProvider>
   );
